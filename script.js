26b8e2b7d325fc5f7e5c6c3889f4785d0a7dd3ce
Ale-M94 <175752593+Ale-M94@users.noreply.github.com>
2024-08-27 15:00:09 -0300

    Get random colours and progressive opacity when mouseover

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const container = document.querySelector("#container");
+
+function randomColor(){
+    let x = Math.floor(Math.random()* 255);
+    let y = Math.floor(Math.random()* 255);
+    let z = Math.floor(Math.random()* 255);
+    return "rgb(" + x + ", " + y + ", " + z + ")";
+}
+
+
 for (let i = 0; i < 256; i++){
     const grid = document.createElement("div");
-    // grid.textContent = i;
     grid.classList.add("grid");
     grid.style.width = "30px";
     grid.style.height = "30px";
     grid.style.border = "1px solid black";
     container.appendChild(grid);
 
+    let opacity = 0;
     grid.addEventListener("mouseover", () =>{
-        grid.style.backgroundColor = "red";
+        grid.style.backgroundColor = randomColor();
+        if(opacity < 1){
+            opacity += 0.10;
+            grid.style.opacity = opacity;
+        }
     });
 };
+
 
 const btn = document.querySelector("#btn");
 
@@ -23,7 +37,6 @@ function newGrid(){
         grids.forEach(grid => grid.remove());
         for (let i = 0; i < userGrid; i++){
             const grid = document.createElement("div");
-            // grid.textContent = i;
             grid.classList.add("grid");
             grid.style.width = "30px";
             grid.style.height = "30px";
@@ -31,7 +44,11 @@ function newGrid(){
             container.appendChild(grid);
         
             grid.addEventListener("mouseover", () =>{
-                grid.style.backgroundColor = "coral";
+                grid.style.backgroundColor = randomColor();
+                if(opacity < 1){
+                    opacity += 0.10;
+                    grid.style.opacity = opacity;
+                }
             });
         }
         }else{
